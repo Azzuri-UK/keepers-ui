6675d8866dc898481b399510ac41db7d083abd41
Justin Carter <aws@justin-carter.co.uk>
2019-12-20 08:07:20 +0000

    added quality to recent loot

--- src/components/Loot/RecentDrops.vue
+++ src/components/Loot/RecentDrops.vue
@@ -10,7 +10,7 @@
             <tbody>
             <tr class="recentTable" v-for="item in loot" :key="item.loot_id">
                 <td width="50%"><div style="font-weight: bold" :class="'wow_' + item.character_class.toLowerCase()">{{ item.character_name }}</div></td>
-                <td><a :href="'https://classic.wowhead.com/item=' + item.loot_id" style="font-weight: bold;text-decoration: none" class="q4" :data-wowhead=item.loot_id>{{item.item_name}}</a></td>
+                <td><a :href="'https://classic.wowhead.com/item=' + item.loot_id" style="font-weight: bold;text-decoration: none" :class="getItemClass(item.item_quality)" :data-wowhead=item.loot_id>{{item.item_name}}</a></td>
             </tr>
             </tbody>
         </template>
@@ -40,7 +40,21 @@
         methods: {
             getItemLink: () => {
                 return ''
-            }
+            },
+            getItemClass: function (item_class) {
+                switch (item_class) {
+                    case 1:
+                        return 'q1';
+                    case 2:
+                        return 'q2';
+                    case 3:
+                        return 'q3';
+                    case 4:
+                        return 'q4';
+                    case 5:
+                        return 'q5'
+                }
+            },
         }
     }
 
