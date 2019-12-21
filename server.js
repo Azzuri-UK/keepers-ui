eec839b9a2172a51eb4427369bdfc83842d7d4e9
Justin Carter <aws@justin-carter.co.uk>
2019-12-21 12:33:07 +0000

    Added rewrites for express

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+const rewrite = require('express-urlrewrite');
+
+
+app.use(rewrite('/loot*', '/'));
+app.use(rewrite('/raids*', '/'));
+app.use(rewrite('/raid*', '/'));
+app.use(rewrite('/roster*', '/'));
+app.use(rewrite('/crafting*', '/'));
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
